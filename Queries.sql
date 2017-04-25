96a7f29497c748850c1da496317e0251b29a47a6
Praga24 <pragadeeshbas@gmail.com>
2017-04-26 00:08:50 +0530

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -19,7 +19,7 @@ SELECT street,city,price FROM real_estate WHERE state=="CA" AND type=="Condo" OR
 
 --List top 5 residency details which lie in the budget of 60000-120000, an area more than 1450, sold after 17th may, min bedroom 3 and, min bathroom 2.
 
-SELECT * FROM real_estate WHERE price>=60000 AND price<=120000 AND sq_ft>1450 AND beds>=3 AND baths>=2 AND  LIMIT 5; 
+SELECT * FROM real_estate WHERE price>=60000 AND price<=120000 AND sq_ft>1450 AND beds>=3 AND baths>=2 ORDER BY price LIMIT 5; 
 
 --Query to separate list of residential apartments with more than 2 beds. Also include columns in following order City,Baths,Sq_feet,Price,flat_type,Beds respectively
 
